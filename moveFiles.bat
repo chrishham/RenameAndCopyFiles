90fc7cdcb518a51fa2ad4469d0404153a43de368
chamaletsos <chrishham@yahoo.gr>
2019-02-05 08:14:55 +0200

    change date substrings, to conform to windows 8 specs

--- moveFiles.bat
+++ moveFiles.bat
@@ -1,14 +1,19 @@
 @echo off
+REM In production replace %~dp0(current path) with real servers
 set SourceServer=%~dp0
 set SourcePath=\SourceFolder\
 set DestinationServer=%~dp0
 set DestinationPath=\DestFolder\
 
-set dateString=%date:~6,4%%date:~3,2%%date:~0,2%
+set dateString=%date:~10,4%%date:~7,2%%date:~4,2%
 
 Setlocal EnableDelayedExpansion
 for %%F in (%SourceServer%%SourcePath%*) do (
-    set oldFileName=%%~nxF
-    set newFileName=!oldFileName:~0,9!!dateString!_!oldFileName:~9!
+  set oldFileName=%%~nxF
+  set part1=!oldFileName:~0,9!
+  if !part1!==EKO2_011_ (
+    set newFileName=!part1!!dateString!_!oldFileName:~9!
+    REM In production replace copy with move
     copy %%F !DestinationServer!!DestinationPath!!newFileName!
+  )
 )
